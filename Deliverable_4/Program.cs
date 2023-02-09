d99954f30b1e88b915fd1425b566d64f54b98936
potot <potot@ROSINANTE>
2023-02-09 21:10:14 +1000

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Deliverable_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[25];
+            
+            fib[0] = 0;
+            fib[1] = 1;
+            
+            for (int i = 2; i < 25; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            for (int i = 0; i < 25; i++)
+                Console.WriteLine("Fibonacci(" + i + ") = " + fib[i]);
+        }
+    }
+}
